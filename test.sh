598bf52982b740a1211513a04dd47d75e5aeac11
lyp830414 <lyp830414@163.com>
2019-05-06 06:14:33 +0000

    update

--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@
 cli_path=/home/lyp/new_baic_chain/Baic-Chain/build/programs/baic_cli
 walletpwd=""
 
-
 function base_new_wallet() {
 
 	rm  ~/baic-wallet/baic.wallet
@@ -78,9 +77,16 @@ function raise_token() {
 
 function system_new_accounts() {
 	new_user=$1
-	token_name=$2
-	echo "$cli_path/./baic_cli system newaccount --transfer --stake-net "100.000000000 $token_name" --stake-cpu "100.000000000 $token_name" --buy-ram "100.000000000 $token_name" baic $new_user BAIC6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV BAIC6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV"
-	$cli_path/./baic_cli system newaccount --transfer --stake-net "100.000000000 $token_name" --stake-cpu "100.000000000 $token_name" --buy-ram "100.000000000 $token_name" baic $new_user BAIC6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV BAIC6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
+	
+	stake_net_amounts="$2 $3"
+	stake_cpu_amounts="$4 $5"
+	buy_ram_amounts="$6 $7"
+
+	echo $buy_ram_amounts
+	
+	echo "$cli_path/./baic_cli system newaccount --transfer --stake-net "$stake_net_amounts" --stake-cpu "$stake_cpu_amounts" --buy-ram "$buy_ram_amounts" baic $new_user BAIC6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV BAIC6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV"
+	
+	$cli_path/./baic_cli system newaccount --transfer --stake-net "$stake_net_amounts" --stake-cpu "$stake_cpu_amounts" --buy-ram "$buy_ram_amounts" baic $new_user BAIC6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV BAIC6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
 }
 
 function get_asset() {
@@ -89,10 +95,80 @@ function get_asset() {
 	$cli_path/./baic_cli get currency balance baic.token $user_name
 }
 
+function check_amount_valid() {
+
+	amount=`eval echo $@`
+
+	echo "check_amount_valid: amount:$amount"
+	if [ ${#amount} -lt 1 ]; then
+		echo "Wrong amount. Please try again."
+		exit
+	fi
+	
+	cnt=0
+	#cnt=`echo $amount| awk '$s+=gsub(/\./,"")'`
+	cnt=`echo $amount | awk -F'.' '{print NF-1}'`
+
+	if [ ${#cnt} -lt 1 ] || [ $cnt -ne 1 ]; then
+		#echo ${#cnt} $cnt
+		echo "Input wrong. Please ensure the amount format is like 1.000000000 DUSD"
+		exit
+	fi
+
+	#idx_start=`awk 'BEGIN {print index("'$amount'", ".")}'`
+	#idx_end=`awk 'BEGIN {print index("'$amount'", " ")}'`
+	#len=`expr idx_end - idx_start`
+	#echo ${amount:$idx : $len}
+	#exit
+
+	
+	#calculate for zero count
+	#zero_cnt=`echo $amount|awk  '{s+=gsub(/0/,"&")}END{print s}'`
+	
+	#calculate for decimal digit numbers
+	decimal_cnts=`echo $amount|awk 'BEGIN{FS="[. ]"} {print length($2)}'`
+	
+	#calcuate for blank count
+	blank_cnts=`eval echo $amount|awk  '{s+=gsub(/ /,"&")}END{print s}'`
+	
+	#echo $zero_cnt $blank_cnt
+	if [ $decimal_cnts -ne 9 ]; then
+		echo "Wrong! Decimal cnt of amount must equals to 9 -> i.e 1.000000000"
+		exit
+	elif [ $blank_cnts -ne 1 ]; then
+		echo $blank_cnts
+		echo "Wrong! your amount must be format like: 1.000000000 <token name>"
+		exit
+	fi
+
+	#calculate for values before token name
+	#idx_start=`echo $amount|awk  '{s+=gsub(/\./,"&")}END{print s}'`
+	#echo $idx_start
+	#awk_param='BEGIN {print index("'$amount'", " ")}'
+	#idx_end=`echo $amount|awk  "$awk_param"`
+	#len=`expr $idx_end - $idx_start`
+	#echo ${amount:$idx : $len}
+	#exit
+
+
+	#idx_start=`awk 'BEGIN {print index("'$amount'", ".")}'`
+	#echo $idx_start
+	#idx_end=`echo $amount|awk 'BEGIN {print index("'$amount'", ".")}'`
+	#echo $idx_end
+	#cnt_zero=echo ${amount:$idx_start}|awk  '{s+=gsub(/0/,"&")}END{print s}'
+	#echo $amount|awk  '{s+=gsub(/ /,"&")}END{print s}'
+	#exit
+	#len=`expr $idx_end - $idx_start`
+	#echo ${amount:$idx : $len}
+	#exit
+
+}
+
+
 function print_help() {
 
 	echo "parameters:"
-	echo "init | get | publish | newaccount"
+	echo "init | get | publish | newaccount | transfer"
 }
 
 function transfer_cash() {
@@ -102,7 +178,7 @@ function transfer_cash() {
 	amount=$4
 	memo=$5
 	if [ $choice_which == "use_baic.token" ]; then
-		param='["baic", "lypabc", "'$amount'”,"vote"]'
+		param='["baic", "lypabc", "'$amount'","vote"]'
 		#echo "$param"
 		echo "$cli_path/./baic_cli push action baic.token transfer "$param" -p baic"
 		$cli_path/./baic_cli push action baic.token transfer "$param" -p baic
@@ -181,20 +257,38 @@ case $input_param in
 		echo "please input your new account:"
 		username=""
 		read username
-		echo "please input your token name(default is DUSD)"
-		token_name="DUSD"
-		read token_name
-		if [ ${#token_name} -lt 1 ]; then
-			token_name="DUSD"
-			echo "use DUSD as your token"
-		else
-			echo "use $token_name as your token."
-		fi
+		#echo "please input your token name(default is DUSD)"
+		#token_name="DUSD"
+		#read token_name
+		#if [ ${#token_name} -lt 1 ]; then
+		#	token_name="DUSD"
+		#	echo "use DUSD as your token"
+		#else
+		#	echo "use $token_name as your token."
+		#fi
+	
+		echo "input your stake-net amount, i.e 3.000000000 DUSD"
+		read stake_net_amounts
 
+		check_amount_valid $stake_net_amounts
+		
+		echo "input your stake-cpu amount, i.e 3.000000000 DUSD"
+		read stake_cpu_amounts
+		check_amount_valid $stake_cpu_amounts
+
+		echo "input your buy-ram amount, i.e 3.000000000 DUSD"
+		read buy_ram_amounts
+		check_amount_valid $buy_ram_amounts
+
+		#echo $stake_net_amount "|" $stake_cpu_amount "|"  $buy_ram_amount
+		
 		echo "+++SYS NEW ACCOUNT: $username ++++"
 		echo
+
 		base_new_wallet
-		system_new_accounts $username $token_name
+		
+		system_new_accounts $username $stake_net_amounts $stake_cpu_amounts $buy_ram_amounts
+
 		exit
 	;;
 	transfer)
@@ -216,63 +310,8 @@ case $input_param in
 		fi
 		echo "input your amount(i.e "2.000000000 DUSD")"
 		read amount
-		cnt=0
-		#cnt=`echo $amount| awk '$s+=gsub(/\./,"")'`
-		cnt=`echo $amount | awk -F'.' '{print NF-1}'`
-		
-
-		if [ ${#cnt} -lt 1 ] || [ $cnt -ne 1 ]; then
-			echo ${#cnt} $cnt
-			echo "Input wrong. Please ensure the amount format is like 1.000000000 DUSD"
-			exit
-		fi
-
-		#idx_start=`awk 'BEGIN {print index("'$amount'", ".")}'`
-		#idx_end=`awk 'BEGIN {print index("'$amount'", " ")}'`
-		#len=`expr idx_end - idx_start`
-		#echo ${amount:$idx : $len}
-		#exit
-	
-		
-		#calculate for zero count
-		#zero_cnt=`echo $amount|awk  '{s+=gsub(/0/,"&")}END{print s}'`
-		
-		#calculate for decimal digit numbers
-		decimal_cnts=`echo $amount|awk 'BEGIN{FS="[. ]"} {print length($2)}'`
-		
-		#calcuate for blank count
-		blank_cnts=`eval echo $amount|awk  '{s+=gsub(/ /,"&")}END{print s}'`
-		
-		#echo $zero_cnt $blank_cnt
-		if [ $decimal_cnts -ne 9	]; then
-			echo "Wrong! Decimal cnt of amount must equals to 9 -> i.e 1.000000000"
-			exit
-		elif [ $blank_cnts -ne 1 ]; then
-			echo "Wrong! your amount must be format like: 1.000000000 <token name>"
-			exit
-		fi
-			
-
-		#calculate for values before token name
-		#idx_start=`echo $amount|awk  '{s+=gsub(/\./,"&")}END{print s}'`
-		#echo $idx_start
-		#awk_param='BEGIN {print index("'$amount'", " ")}'
-		#idx_end=`echo $amount|awk  "$awk_param"`
-		#len=`expr $idx_end - $idx_start`
-		#echo ${amount:$idx : $len}
-		#exit
-
-
-		#idx_start=`awk 'BEGIN {print index("'$amount'", ".")}'`
-		#echo $idx_start
-		#idx_end=`echo $amount|awk 'BEGIN {print index("'$amount'", ".")}'`
-		#echo $idx_end
-		#cnt_zero=echo ${amount:$idx_start}|awk  '{s+=gsub(/0/,"&")}END{print s}'
-		#echo $amount|awk  '{s+=gsub(/ /,"&")}END{print s}'
-		#exit
-		#len=`expr $idx_end - $idx_start`
-		#echo ${amount:$idx : $len}
-		#exit
+		echo "check:$amount"
+		check_amount_valid $amount	
 
 		echo "input your memo"
 		read memo
@@ -280,6 +319,7 @@ case $input_param in
 		choice_which="use_baic.token"
 		#choice_which="use orignal"
 
+		base_new_wallet
 		transfer_cash $choice_which $from $to "$amount" "$memo"
 	;;	
 	*) #default case
